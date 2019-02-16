bdd4c358a1fa0e0612a929819b045bae486fb16c
Nima Seyedtalebi <nseyedtalebi@gmail.com>
2019-02-16 13:29:31 -0500

    Cleaned up the code, began making changes to prepare for real sim

--- runmodels.py
+++ runmodels.py
@@ -2,6 +2,7 @@ import networkx as nx
 import numpy as np
 import steiner_ilp
 from networkx.algorithms.approximation.steinertree import steiner_tree
+from networkx.classes.graphviews import subgraph_view
 from random import sample,randint
 from collections import OrderedDict
 
@@ -12,50 +13,42 @@ def find_optimal_stage(V,E,T,weights,capacities,required):
             sv,se,models[v] = steiner_ilp.bidirected_steiner_tree(set(V),set(E),set(T).union(set((v,))),weights=weights)
     return models[min(models,key=lambda m:float(models[m].getAttr('objval')))],models
 
-def opt_stage(V,E,T,weights,capacities,required):
+def opt_stage(model,terminals,required_capacity):
     trees = {}
-    for v in V:
-        if capacities[v] >= required:
-            g = nx.Graph()
-            g.add_nodes_from(V)
-            for e in E:
-                g.add_edge(*e,weight=weights[e])
-            trees[v] = steiner_tree(g,T)
-    return OrderedDict(sorted(trees.items(),key=lambda t:total_weight(t[1].edges,\
-    weights)))
+    for v in model.nodes:
+        if model.nodes[v]['capacity'] >= required_capacity:
+            trees[v] = steiner_tree(model,terminals)
+    #return OrderedDict(sorted(trees.items(),key=lambda t:(total_weight(t[1]))))
+    return trees
 
-def total_weight(E,weights):
-    return sum((weights[e] for e in E))
-#graph_size = 32 ran out of memory
-graph_size=128
-u = nx.complete_graph(n=graph_size)
-slow_edge = 100
-fast_edge=  1
-num_fast = 2
-num_terminals = 2
-fast_edges = sample(tuple(u.edges),num_fast)
-terminals = sample(list(u.nodes()),num_terminals)
-ccap_low = 1 
-ccap_hi = 10
+def total_weight(graph):
+    return sum((graph.edges[edge]['weight'] for edge in graph.edges))
+    
+def get_model(graph_size,slow_edge,fast_edge,num_fast,num_terminals,ccap_low,
+ccap_hi):
+    u = nx.complete_graph(n=graph_size)
+    fast_edges = sample(tuple(u.edges),num_fast)
+    for edge in u.edges:
+        u.add_edge(*edge,weight=fast_edge if edge in fast_edges else slow_edge)
+    for node in u.nodes:
+        u.nodes[node]['capacity']=randint(ccap_low,ccap_hi)
+    weights = {}
+    for i in range(len(u)):
+        for j in range(len(u)):
+            if i!=j:
+                weights[i,j] = u.get_edge_data(i,j)['weight']
+    #uniform distr capacities
+    capacities = u.nodes.data()
+    return u,weights,capacities;
 
-for edge in u.edges:
-    u.add_edge(*edge,weight= fast_edge if edge in fast_edges else slow_edge)
-weights = {}
-for i in range(len(u)):
-    for j in range(len(u)):
-        if i!=j:
-            weights[i,j] = u.get_edge_data(i,j)['weight']
 
-#st = steiner_tree(u,terminals)
-
-#sv,se,mdl = steiner_ilp.bidirected_steiner_tree(set(u.nodes),set(u.edges),terminals,weights)
-
-#uniform distr capacities
-capacities = {v:randint(ccap_low,ccap_hi) for v in u.nodes}
-required_capacity = 3
-trees = opt_stage(u.nodes,u.edges,set((2,6)),weights,capacities,required_capacity)
 #best_model,models = find_optimal_stage(u.nodes,u.edges,set((2,6)),weights,capacities,required_capacity)
- 
+#graph_size = 32 ran out of memory
+model_params = {'graph_size':32,'slow_edge':100,'fast_edge':1,'num_fast':10,
+    'num_terminals':2,'ccap_low':1,'ccap_hi':10}
+model,weights,capacities = get_model(**model_params)
+required_capacity = 3
+trees = opt_stage(model,set((2,6)),required_capacity)
 
 #
 """TODO
